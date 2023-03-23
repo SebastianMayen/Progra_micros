d4f6a7b594d74eb2de3cac8e1396711459f06679
SebastianMayen <125109218+SebastianMayen@users.noreply.github.com>
2023-03-22 23:20:54 -0600

    Add files via upload

---
+++ Ejercicio 5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	int numeros[10];
+	int *puntero = numeros;
+
+	for (int i = 0; i < 10; i++) {
+		printf("Ingresa un numero %d: ", i);
+		scanf("%d", &numeros[i]);
+	}
+	
+	printf("Los elementos del array multiplicados por 2 son:\n");
+	for (int i = 0; i < 10; i++) {
+		printf("%d\n", *puntero * 2);
+		puntero++;
+	}
+	printf("\n");
+	
+	return 0;
+}
+
